8bd95436d48510dbf731fd772e779141ae21ad87
Cooper Johnson <lavosprime@gmail.com>
2014-11-02 16:12:08 -0800

    Create skeletal Main.cc

---
+++ Main.cc
@@ -0,0 +1,15 @@
+/*******************************************************************************
+
+  Copyright (c)  2014 by Cooper Johnson <lavosprime@gmail.com>
+  This program is free software provided under the terms of the MIT License.
+
+  Main.cc: Setup, teardown, and main game loop.
+
+*******************************************************************************/
+
+#include <cstdlib>
+#include <ncurses.h>
+
+int main(void) {
+  return 0;
+}
